a56511adf16aa6f26b5a38845d0173472605b6a1
gowthamraina483 <31271767+gowthamraina483@users.noreply.github.com>
2017-09-03 18:00:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function(req,res){
 });
 
 app.get('/article-two', function(req,res){
-    res.send('article two will be served here');
+    res.sendfile(path.join(__dirname, 'ui' , 'article-two.html'));
 });
 
 app.get('/article-three', function(req,res){
